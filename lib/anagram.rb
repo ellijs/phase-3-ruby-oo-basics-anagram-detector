b51a8f68466caeb2531efc0e77ba33e8c5131c33
ellijs <ellijs@hotmail.com>
2021-08-09 23:53:36 -0400

    Completed Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.filter {|word| word.chars.sort == @word.chars.sort}
+    end
+end
